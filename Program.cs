cd714c0e00a80ffc13fcee7396274e3a2bd8a1bb
Alex <alex.dennett@students.plymouth.ac.uk>
2023-02-09 16:44:13 +0000

    Added basic functions

--- Program.cs
+++ Program.cs
@@ -14,6 +14,54 @@ namespace Report
         }
 
 
+        public bool Is_Empty(List<int> S)
+        {
+            if (S.Count == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public int Size(List<int> S)
+        {
+            return S.Count;
+        }
+
+        public int Capacity(List<int> S)
+        {
+            return S.Capacity;
+        }
+
+        public bool Is_Element_Of(int x, List<int> S)
+        {
+            if (S.Contains(x))
+                return true;
+            else
+                return false;
+        }
+
+        public void Print(List<int> S)
+        {
+            for (int i = 0; i < S.Count; i++)
+                Console.WriteLine(S[i]);
+        }
+
+        public (List<int> S)
+        {
+            return;
+        }
+
+        public List<int> Is_Empty(List<int> S)
+        {
+            return;
+        }
+
+        public List<int> Is_Empty(List<int> S)
+        {
+            return;
+        }
+
+
 
         static void Main(string[] args)
         {
