e18c420f488b6e6ff78324aab241309df6682ced
Alex <alex.dennett@students.plymouth.ac.uk>
2023-02-09 16:22:03 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Report
+{
+    class Program
+    {
+
+
+        public List<int> Clear_Set(List<int> S)
+        {
+            return new List<int>();
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+
+            List<int> S = new List<int>();
+            List<int> T = new List<int>();
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
